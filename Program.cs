39b5b6934b9982dd27062bd9a6b34bf74185c98c
DivyaMishra6 <divyamishra599@gmail.com>
2023-03-29 17:33:22 +0530

    Call Deqeue Method

--- Program.cs
+++ Program.cs
@@ -8,10 +8,17 @@ namespace StackAndQueueDay14
         {
             Console.WriteLine("Welcome to Stack And Queue Program!");
             Console.WriteLine("#####################");
-            QueueWithLinkedList obj = new QueueWithLinkedList();
+            CreateDeqeue obj = new CreateDeqeue();
             obj.Enqueue(56);
             obj.Enqueue(30);
             obj.Enqueue(70);
+            //obj.Display();
+
+            Console.WriteLine("Element stored in queue is:-");
+            obj.Display();
+            Console.WriteLine("After Dequeue operation:");
+            obj.Dequeue();
+            Console.WriteLine("Element stored in queue is:-");
             obj.Display();
 
         }
