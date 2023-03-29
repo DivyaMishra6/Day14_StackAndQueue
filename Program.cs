37afa25e2b4d792cbc42b41bd4dc003b5703f094
DivyaMishra6 <divyamishra599@gmail.com>
2023-03-29 17:12:39 +0530

    Add value in Queue

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace StackAndQueueDay14
+﻿using StackAndQueue_day14;
+
+namespace StackAndQueueDay14
 {
     public class Program
     {
@@ -6,7 +8,11 @@
         {
             Console.WriteLine("Welcome to Stack And Queue Program!");
             Console.WriteLine("#####################");
-
+            QueueWithLinkedList obj = new QueueWithLinkedList();
+            obj.Enqueue(56);
+            obj.Enqueue(30);
+            obj.Enqueue(70);
+            obj.Display();
 
         }
 
