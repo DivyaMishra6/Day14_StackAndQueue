2b5f678e79fc2486f7e69e3cad51cc57ab0f069a
DivyaMishra6 <divyamishra599@gmail.com>
2023-03-29 15:51:36 +0530

    Main code

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace StackAndQueueDay14
+﻿using StackAndQueue_day14;
+
+namespace StackAndQueueDay14
 {
     public class Program
     {
@@ -6,7 +8,11 @@
         {
             Console.WriteLine("Welcome to Stack And Queue Program!");
             Console.WriteLine("#####################");
-
+            UC1_CreateStack obj = new UC1_CreateStack();
+            obj.push(70);
+            obj.push(30);
+            obj.push(56);
+            obj.Display();
 
         }
 
