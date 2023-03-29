f62ae4c08070e17f0954d222aa20b3430c002ab4
DivyaMishra6 <divyamishra599@gmail.com>
2023-03-29 16:35:29 +0530

    Program

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace StackAndQueueDay14
+﻿using StackAndQueue_day14;
+
+namespace StackAndQueueDay14
 {
     public class Program
     {
@@ -7,7 +9,16 @@
             Console.WriteLine("Welcome to Stack And Queue Program!");
             Console.WriteLine("#####################");
 
-
+            PeekAndPopUC2 obj = new PeekAndPopUC2();
+            obj.push(70);
+            obj.push(30);
+            obj.push(56);
+            obj.Display();
+            //obj.Peek();
+            Console.WriteLine("After pop operation:");
+            //obj.Pop();
+            obj.IsEmpty();
+            obj.Display();
         }
 
     }
