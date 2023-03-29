24c575d70c4b0a47c2f0a96bb1a9c1e99cfaa481
DivyaMishra6 <divyamishra599@gmail.com>
2023-03-29 17:34:17 +0530

    next node

--- node.cs
+++ node.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace StackAndQueue_day14
 {
-    internal class node
+    public class Node
     {
+        public int data;
+        public Node next;
+        public Node(int data)
+        {
+            this.data = data;
+        }
     }
 }
